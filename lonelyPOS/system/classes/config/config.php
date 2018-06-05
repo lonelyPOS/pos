a76ba30a348db1b63de11030f0e0fec64f490384
pakon <pakon@DomePakornNB>
2018-06-05 21:09:43 +0700

    delete file

--- lonelyPOS/system/classes/config/config.php
+++ lonelyPOS/system/classes/config/config.php
@@ -1,7 +1,7 @@
 <?php
   //  $hostname = '192.168.1.10:3307'; //inside
     $hostname = 'server.mydpk.net:3307'; //outside
-    $username = 'ooomg';
-    $password = '12345678';
-    $dbname = 'ooomg';
+    $username = 'lonely';
+    $password = 'lonely';
+    $dbname = 'lonely';
 ?>

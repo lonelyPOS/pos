f585f6ee5a40bff2747fd0534933bba321d80763
pattarabank <pattarabank@172.25.65.238>
2018-06-10 21:53:24 +0700

    eiei

--- lonelyPOS/includes/menu.php
+++ lonelyPOS/includes/menu.php
@@ -29,7 +29,7 @@
 					<a class="js-arrow" href="#"> <i class="fas fa-users"></i>Member
 				</a>
 					<ul class="list-unstyled navbar__sub-list js-sub-list">
-						<li><a href="#"><i class="fas fa-list"></i>List Member</a></li>
+						<li><a href="listmember.php"><i class="fas fa-list"></i>List Member</a></li>
 						<li><a href="#"><i class="fas  fa-plus"></i>Add Member</a></li>
 					</ul>
 				</li>
